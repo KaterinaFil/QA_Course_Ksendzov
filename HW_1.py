b5771df0f99ae4033450fa403c97330994055eda
KaterinaFil <sailorkate2@gmail.com>
2021-11-07 20:07:55 +0300

    Create .py files

---
+++ HW_1.py
@@ -0,0 +1,60 @@
+# Python_2. HW_1
+
+# 1) Создать переменную типа String.
+name = 'Katerina'
+print('Name:', name)
+print(type(name))
+
+# 2) Создать переменную типа Integer
+age = 27
+print('Age:', age)
+print(type(age))
+
+# 3) Создать переменную типа Float
+ageFloat = 27.8
+print('Age1:', ageFloat)
+print(type(ageFloat))
+
+# 4) Создать переменную типа Bytes
+typeBytes = bytes(250)
+print(typeBytes)
+print(type(typeBytes))
+
+# 5) Создать переменную типа List
+listType = ['Katerina', 123, True, '27', ['316'] ]
+print(listType)
+print(type(listType))
+
+# 6) Создать переменную типа Tuple
+tupleType = ('Kate', 2502, False, '333')
+print(tupleType)
+print(type(tupleType))
+
+# 7) Создать переменную типа Set
+s= set('Hello')
+print(s)
+print(type(s))
+
+# 8) Создать переменную типа Frozen set
+f = frozenset('Goodbye')
+print(f)
+print(type(f))
+
+# 9) Создать переменную типа Dict
+dict_type = {'number1':'Kate','number2':'Maria'}
+print(dict_type)
+print(type(dict_type))
+
+# 11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные.
+# Вывести в консоль.
+n= 'Good'
+m= 'Morning'
+print(n+m)
+
+# 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+print('Age:', 27)
+
+# 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
+print('Year: ' + str(2021))
+
+
